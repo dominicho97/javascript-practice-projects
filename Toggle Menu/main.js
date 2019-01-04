f5235781db2c2c35529e4c7983e835a763776b98
dominicho97 <dominic.ho97@gmail.com>
2019-01-04 15:58:32 +0100

    added let toggleStatusav, let toggleNav+quesrySelectors

---
+++ Toggle Menu/main.js
@@ -0,0 +1,16 @@
+
+
+let toggleNavStatus = false;
+
+let toggleNav = function(){
+  let getSideBar = document.querySelector(".nav-sidebar ");
+  let getSideBarUl = document.querySelector(".nav-sidebar ul");
+  let getSideBarTitle = document.querySelector(".nav-sidebar span");
+  let getSideBarLinks = document.querySelectorAll(".nav-sidebar a");
+
+
+if (toggleNavStatus === false){
+
+}
+
+}
