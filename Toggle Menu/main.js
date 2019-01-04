a15602cc0c8555c3a380be5773f8bcea5f3bc91a
dominicho97 <dominic.ho97@gmail.com>
2019-01-04 16:37:16 +0100

    added else-if statement

--- Toggle Menu/main.js
+++ Toggle Menu/main.js
@@ -17,8 +17,27 @@ let toggleNav = function(){
 
   let arrayLength = getSidebarLinks.length;
   for (let i = 0; i < arraylength; i++) {
-  getSidebarLinks[0].style.opacity = "1";
+  getSidebarLinks[i].style.opacity = "1";
       
     }
+
+    toggleNavStatus = true;
+  }
+
+  else if (toggleNavStatus === true){
+      
+      getSidebar.style.width = "50px";
+      getSidebarTitle.style.opacity = "0";
+    
+    
+      let arrayLength = getSidebarLinks.length;
+      for (let i = 0; i < arraylength; i++) {
+      getSidebarLinks[i].style.opacity = "0";
+    }
+
+    getSidebarUl.style.visibility = "hidden";
+    toggleNavStatus = false;
   }
 }
+
+
