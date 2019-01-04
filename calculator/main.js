67880e20bd751e78545bda9bcf0267e29795fea1
dominicho97 <dominic.ho97@gmail.com>
2019-01-04 11:32:07 +0100

    added id's to the variables in html , added funtion calc, and converted querySelectors from string to numbs with parseInt

---
+++ calculator/main.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+function calc(){
+  /*parseInt converts the string we take from html into numbers*/
+  var a = parseInt(document.querySelector("#value1").value);
+  var a = parseInt(document.querySelector("#value1").value);
+}
+}
