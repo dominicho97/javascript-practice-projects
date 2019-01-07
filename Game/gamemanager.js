4cdad699974cb31cad09ff4b591e16f0f1ccc630
dominicho97 <dominic.ho97@gmail.com>
2019-01-07 09:38:16 +0100

    added switch-statement and let getInterface,getInterface.innerHTML

--- Game/gamemanager.js
+++ Game/gamemanager.js
@@ -6,7 +6,23 @@ let GameManager = {
 
   },
   resetPlayer: function(classType) {
-
+    switch (classType) {
+      case "Warrior":
+        player = new Player(classType, 200, 0, 200, 100, 50);
+        break;
+      case "Rogue":
+        player = new Player(classType, 100, 0, 100, 150, 200);
+        break;
+      case "Mage":
+        player = new Player(classType, 80, 0, 50, 200, 50);
+        break;
+        case "Hunter":
+        player = new Player(classType, 200, 0, 50, 200, 100);
+        break;
+    }
+    let getInterface = document.querySelector(".interface");
+     getInterface.innerHTML = '<img src = "img/avatar-player/' + classType.toLowerCase() + '.png class="img-avatar" "><div><h3>' + classType
+      +'</h3><p>Health: ' + player.health + ' </p>' ;   
 },
 setPreFight: function() {
 
