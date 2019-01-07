aa6d962204ff3e2f0b14c9219131d7f7743ebddd
dominicho97 <dominic.ho97@gmail.com>
2019-01-07 12:02:12 +0100

    changes javascript file to scripts.js

--- simple slideshow/scripts.js
+++ simple slideshow/scripts.js
@@ -2,11 +2,27 @@
 
 
 
-let i = 0;
-let images = {};
-let time = 3000;
+var i = 0;  // start point
+var images = {};
+var time = 3000;
 
-//image lists
-images[0] = "hunter.jpg";
-images[1] = "warrior.jpg";
-images[2] = "mage.jpg";
+//image list
+images[0] = "image1.jpg";
+images[1] = "image2.jpg";
+images[2] = "image3.jpg";
+
+//change image
+function changeImg (){
+document.slide.src = images[i];
+
+if (i < images.length -1){
+  ++i
+} else {
+  i = 0
+}
+
+
+setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
