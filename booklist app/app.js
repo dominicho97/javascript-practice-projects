1081b5251b9acedd97ab3ac044c4cdcc5a497721
dominicho97 <dominic.ho97@gmail.com>
2019-01-08 13:59:25 +0100

    added const container,form in statis showAlert

--- booklist app/app.js
+++ booklist app/app.js
@@ -62,6 +62,11 @@ class UI{
 
     static showAlert(message, className) {
       const div = document.createElement('div');
+      div.className = `alert alert-${className}`;
+      div.appendChild(document.createTextNode(message));
+      const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
   }
 
 
