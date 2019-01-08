a905eb86795f42e036edf43e6504c26119641577
dominicho97 <dominic.ho97@gmail.com>
2019-01-08 11:37:16 +0100

    added book vlass

---
+++ booklist app/app.js
@@ -0,0 +1,30 @@
+// Book class: Represents a Book
+
+class Book {
+  constructor(title,author,isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+
+
+
+
+// UI class: Handle UI Tasks
+
+
+
+
+// Storage class: Handles Storage
+
+
+// Eevent: Displays Book
+
+
+// Event: Add a Book
+
+
+
+// Event: Remove a Book
