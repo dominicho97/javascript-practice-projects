7983e9c2e835982ce785d9e4d5d3118b2e7e0045
dominicho97 <dominic.ho97@gmail.com>
2019-01-09 15:09:01 +0100

    finished excercise, added answer and commented code

--- modal/main.js
+++ modal/main.js
@@ -1,4 +1,4 @@
-// Get modal element
+/*// Get modal element
 var modal = document.getElementsByClassName("simpleModal");
 //get open modal button
 var modalBtn = document.getElementsByClassName("modalBtn");
@@ -25,6 +25,34 @@ function closeModal() {
 }
 
 //function to close modal if outside click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
+*/
+
+// Get DOM Elements
+const modal = document.querySelector("#my-modal");
+const modalBtn = document.querySelector("#modal-btn");
+const closeBtn = document.querySelector(".close");
+
+// Events
+modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = "block";
+}
+
+// Close
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Close If Outside Click
 function outsideClick(e) {
   if (e.target == modal) {
     modal.style.display = "none";
