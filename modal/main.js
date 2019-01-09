247502fe9c0bfa9b8cab9c22527348ee2c2cf63e
dominicho97 <dominic.ho97@gmail.com>
2019-01-09 15:03:18 +0100

    added animation name/durtion in .modal-content css, added @keyframes

--- modal/main.js
+++ modal/main.js
@@ -11,6 +11,8 @@ modalBtn.addEventListener("click", openModal);
 // listen for  close Click
 closeBtn.addEventListener("click", closeModal);
 
+// listen for outside click
+window.addEventListener("click", outsideClick);
 //function to open modal
 function openModal() {
   modal.style.display = "block";
@@ -20,4 +22,11 @@ function openModal() {
 
 function closeModal() {
   modal.style.display = "none";
+}
+
+//function to close modal if outside click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
 }
