582a7f9dd39781808c5d9ac31158edc7edec37a6
dominicho97 <dominic.ho97@gmail.com>
2019-01-09 14:39:26 +0100

    added var modal,modalBtn, closeBtn, function openModal

---
+++ modal/main.js
@@ -0,0 +1,13 @@
+// Get modal element
+var modal = document.getElementById("simpleModal");
+//get open modal button
+var modalBtn = document.getElementById("modalBtn");
+// get close button
+var closeBtn = document.getElementById("closeBtn");
+
+// listen for  Click
+modalBtn.addEventListener("click", openModal);
+
+function openModal() {
+  console.log(123);
+}
